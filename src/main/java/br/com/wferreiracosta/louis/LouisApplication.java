2d6d467c6b274f2e09a50b2488fa38365e18942b
Wesley Costa <wesferrcosta@gmail.com>
2023-11-11 20:22:24 -0300

    Initial commit

---
+++ src/main/java/br/com/wferreiracosta/louis/LouisApplication.java
@@ -0,0 +1,13 @@
+package br.com.wferreiracosta.louis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wferreiracosta/louis/LouisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wferreiracosta.louis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
